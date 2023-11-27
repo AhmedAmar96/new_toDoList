75a160f630a0126195097cd904700e896b5f3a1b
AhmedAmar96 <ahmed.amar.aae@gmail.com>
2023-11-27 22:31:54 +0200

    add cors

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+  app.use(cors());
   const config = new DocumentBuilder()
     .setTitle('Your API')
     .setDescription('API description')
